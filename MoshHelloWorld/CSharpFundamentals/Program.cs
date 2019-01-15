480297de50dffee67d110fed66226117cf1ccd5f
raghu <draghavendar@gmail.com>
2019-01-15 14:20:23 -0500

    [ControlFlow] - Need to visit back for review. but I guess i did all the examples taught in Controlflow section.

--- MoshHelloWorld/CSharpFundamentals/Program.cs
+++ MoshHelloWorld/CSharpFundamentals/Program.cs
@@ -67,6 +67,39 @@ This is version controlled";
             Console.WriteLine("array1[0]: {0}, array2[0]: {1}",array1[0],array2[0]);
             #endregion
 
+            #region conditional Statements
+            //  if/else and Switch/case
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It is Mornig" );
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It is evening");
+            }
+            else
+            {
+                Console.WriteLine("I dont know what it is.");
+            }
+
+            // conditional operator
+            var isGoldCustomer = true;
+
+            //using if/else
+            float price;
+            if (isGoldCustomer)
+                price = 19.99f;
+            else
+                price = 29.99f;
+            Console.WriteLine(string.Format("price : {0} ", price));
+
+            //using conditional expression.
+            float price2 = (isGoldCustomer ? 12.99f : 22.99f);
+            Console.WriteLine(string.Format("price2 : {0} " , price2));
+            #endregion
+
             Console.ReadKey();
 
         }
